09002c71de28013815dbc50798d13306a7dce42f
Andrea Giovanni Bianchessi <andrea.g.bianchessi@gmail.com>
2023-01-27 14:57:33 +0100

    improved type checking

--- src/lib/components/HorizontalChart.js
+++ src/lib/components/HorizontalChart.js
@@ -3,38 +3,39 @@
  * @copyright Andrea Giovanni Bianchessi 2023
  * @author Andrea Giovanni Bianchessi <andrea.g.bianchessi@gmail.com>
  * @license MIT
- * @version 0.1.1
+ * @version 0.1.2
  */
 
-import React, { useRef, useEffect } from 'react'
-import PropTypes from 'prop-types'
-import { DataSample, TimeSeries, HorizontalChart as HorizontalChartCore } from './horizontalcharts.lib'
+import React, { useRef, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { DataSample, TimeSeries, HorizontalChart as HorizontalChartCore } from './horizontalcharts.lib';
 
 const HorizontalChart = ({ options, data, isRealTime = false }) => {
 
-    const canvasRef = useRef(null)
+    const canvasRef = useRef(null);
 
     useEffect(() => {
         // Retrieve the canvas
-        const canvas = canvasRef.current
+        const canvas = canvasRef.current;
         // Create the chart
         let chart = new HorizontalChartCore(options, isRealTime);
         chart.addTimeSeries(...data);
         chart.streamTo(canvas);
-    })
+    });
 
     return <canvas ref={canvasRef} style={
         (options.horizontal == null || options.horizontal === true) ?
             { width: "100%" }
             :
             { height: "100%" }
-    } />
+    }
+    />
 
 }
 
 HorizontalChart.propTypes = {
     options: PropTypes.object,
-    data: PropTypes.array,
+    data: PropTypes.arrayOf(PropTypes.instanceOf(TimeSeries)).isRequired,
     isRealTime: PropTypes.bool
 }
 
